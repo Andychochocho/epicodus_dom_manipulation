629d2e066d7bd205e32b4771e94fedf6d1b12402
Anna Becker and Andrew Cho <anna.r.becker@gmail.com>
2016-01-12 09:33:00 -0800

    add pictures to our img directory.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ $(function(){
   $("button#cat").click(function() {
     $("ul#user").prepend("<li>meow</li>")
     $("ul#webpage").prepend("<li>woof</li>")
-    $("ul#user").after("<li>chirp</li>")
+    $("ul#user").after("<img src='img/cat.jpg'>")
     $("ul#webpage").after("<li>chirp</li>")
   });
+
   $("button#dog").click(function() {
     $("ul#webpage").prepend("<li>woof</li>")
     $("ul#user").prepend("<li>meow</li>")
     $("ul#webpage").before("<li>moo</li>")
     $("ul#user").before("<li>moo</li>")
   });
-
 });
